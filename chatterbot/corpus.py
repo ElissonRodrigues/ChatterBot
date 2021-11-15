6c700c29c43663ab21003764e357e84edcbeda35
ElissonRodrigues <elissonrodrigues98@gmail.com>
2021-11-15 06:45:32 -0300

    Update corpus.py

--- chatterbot/corpus.py
+++ chatterbot/corpus.py
@@ -51,7 +51,7 @@ def read_corpus(file_name):
         with io.open(file_name, encoding='utf-8') as data_file:
             return yaml.safe_load(data_file)
     except FileNotFoundError:
-        raise Exception('chatterbot-corpus is not installed, use the command: "pip3 https://github.com/ElissonRodrigues/ChatterBot/archive/refs/heads/master.zip" to install')
+        raise Exception('chatterbot-corpus is not installed, use the command: "pip3 install https://github.com/ElissonRodrigues/ChatterBot/archive/refs/heads/master.zip" to install')
 
 def list_corpus_files(dotted_path):
     """
