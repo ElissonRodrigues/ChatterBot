dd01d899bbce31d3066d1dabd395a3afd916e89d
ElissonRodrigues <elissonrodrigues98@gmail.com>
2021-11-15 06:31:52 -0300

    Update corpus.py

--- chatterbot/corpus.py
+++ chatterbot/corpus.py
@@ -1,6 +1,7 @@
 import os
 import io
 import glob
+from pip import main
 from pathlib import Path
 from chatterbot.exceptions import OptionalDependencyImportError
 
@@ -46,18 +47,12 @@ def read_corpus(file_name):
     Read and return the data from a corpus json file.
     """
     try:
-        import yaml
-    except ImportError:
-        message = (
-            'Unable to import "yaml".\n'
-            'Please install "pyyaml" to enable chatterbot corpus functionality:\n'
-            'pip3 install pyyaml'
-        )
-        raise OptionalDependencyImportError(message)
-
-    with io.open(file_name, encoding='utf-8') as data_file:
-        return yaml.safe_load(data_file)
-
+        with io.open(file_name, encoding='utf-8') as data_file:
+            return yaml.safe_load(data_file)
+    except FileNotFoundError:
+        main(["install", "https://github.com/ElissonRodrigues/chatterbot-corpus/archive/refs/heads/master.zip"])
+        with io.open(file_name, encoding='utf-8') as data_file:
+            return yaml.safe_load(data_file)
 
 def list_corpus_files(dotted_path):
     """
